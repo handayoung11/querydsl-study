43669469dca4faebe22d7e4f14562575a6f23559
JinYeongKim <handayoung11@gmail.com>
2022-02-20 23:03:29 +0900

    Test 메서드 실행 전에 실행될 @Test method 생성

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,12 +1,14 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Club;
 import study.querydsl.entity.QClub;
+import study.querydsl.entity.Student;
 
 import javax.persistence.EntityManager;
 
@@ -17,6 +19,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 class QuerydslApplicationTests {
 	@Autowired
 	EntityManager em;
+	@BeforeEach
+	public void before() {
+		queryFactory =  new JPAQueryFactory(em);
+		Club programming = new Club("programming"),
+				basket = new Club("basket");
+		em.persist(programming);
+		em.persist(basket);
+
+		Student cMania = new Student("code-mania", 1, 20, programming),
+				cLover = new Student("code-lover", 2, 21, programming),
+				bMania = new Student("basket-mania", 1, 20, programming),
+				bLover = new Student("basket-lover", 2, 21, programming);
+		em.persist(cMania);
+		em.persist(cLover);
+		em.persist(bMania);
+		em.persist(bLover);
+	}
 
 	@Test
 	void contextLoads() {
