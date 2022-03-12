921cedf8a0b1d46aa2d3e152296eecb320263627
JinYeongKim <handayoung11@gmail.com>
2022-03-12 19:14:27 +0900

    순수 JPA Repo StudentJPARepository 생성 save - 저장, findById - Id로 검색, findAll - 전체 조회, findByName - 이름으로 필터링 검색

---
+++ src/main/java/study/querydsl/repository/StudentJpaRepository.java
@@ -0,0 +1,42 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Student;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static study.querydsl.entity.QStudent.student;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StudentJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    @Transactional
+    public void save(Student student) {
+        em.persist(student);
+    }
+
+    public Optional<Student> findById(Long id) {
+        return Optional.ofNullable(em.find(Student.class, id));
+    }
+
+    public List<Student> findAll() {
+        return em.createQuery("select s from Student s")
+                .getResultList();
+    }
+
+    public List<Student> findByName(String name) {
+        return em.createQuery("select s from Student s where s.name = :name")
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
