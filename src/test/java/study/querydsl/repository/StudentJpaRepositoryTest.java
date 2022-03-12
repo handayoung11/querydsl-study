bcb36fc6fcd5fce59c9c89fb0aa1456d24c8eef3
JinYeongKim <handayoung11@gmail.com>
2022-03-12 19:15:35 +0900

    StudentJPARepository Test code 작성

---
+++ src/test/java/study/querydsl/repository/StudentJpaRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Student;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class StudentJpaRepositoryTest {
+    @Autowired
+    EntityManager em;
+    @Autowired
+    StudentJpaRepository studentJpaRepository;
+
+    @Test
+    public void basicTest() {
+        Student student = new Student("code-mania", 1, 20, null);
+        studentJpaRepository.save(student);
+
+        Student findStudent = studentJpaRepository.findById(student.getId())
+                .orElseThrow(NullPointerException::new);
+        assertThat(student).isEqualTo(findStudent);
+
+        List<Student> students = studentJpaRepository.findAll();
+        assertThat(students).contains(student);
+
+        List<Student> studentsByName = studentJpaRepository.findByName(student.getName());
+        assertThat(studentsByName).contains(student);
+    }
+}
