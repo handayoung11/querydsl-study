f3a5554c4ae5361e81a64c51eecd479e913b559c
JinYeongKim <handayoung11@gmail.com>
2022-03-14 23:07:22 +0900

    searchByBuilder test code 작성

--- src/test/java/study/querydsl/repository/StudentJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/StudentJpaRepositoryTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.StudentClubDTO;
+import study.querydsl.dto.StudentSearchCondition;
+import study.querydsl.entity.Club;
 import study.querydsl.entity.Student;
 
 import javax.persistence.EntityManager;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,5 +48,29 @@ class StudentJpaRepositoryTest {
 
         List<Student> studentsByName = studentJpaRepository.findByName_DSL(student.getName());
         assertThat(studentsByName).contains(student);
+    }
+
+    @Test
+    public void searchTest() {
+        Club programming = new Club("programming");
+        Club basket = new Club("basket");
+        em.persist(programming);
+        em.persist(basket);
+
+        Student cMania = new Student("code-mania", 1, 20, programming);
+        Student cLover = new Student("code-lover", 2, 21, programming);
+        Student bMania = new Student("basket-mania", 1, 20, basket);
+        Student bLover = new Student("basket-lover", 2, 21, basket);
+        em.persist(cMania);
+        em.persist(cLover);
+        em.persist(bMania);
+        em.persist(bLover);
+
+        StudentSearchCondition condition = new StudentSearchCondition();
+        condition.setMaxAge(20);
+        condition.setClubName(programming.getName());
+
+        List<StudentClubDTO> result = studentJpaRepository.searchByBuilder(condition);
+        assertThat(result).extracting("studentId").contains(cMania.getId());
     }
 }
