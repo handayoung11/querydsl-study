80d11b80c5b5de1d77bf46c935043f3091bbdacf
JinYeongKim <handayoung11@gmail.com>
2022-03-20 23:09:51 +0900

    searchStudentV2 api 작성

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -1,11 +1,14 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.StudentClubDTO;
 import study.querydsl.dto.StudentSearchCondition;
 import study.querydsl.repository.StudentJpaRepository;
+import study.querydsl.repository.StudentRepository;
 
 import java.util.List;
 
@@ -14,9 +17,15 @@ import java.util.List;
 public class MemberController {
 
     private final StudentJpaRepository studentJpaRepository;
+    private final StudentRepository studentRepository;
 
     @GetMapping("/v1/members")
     public List<StudentClubDTO> searchStudentV1(StudentSearchCondition condition) {
         return studentJpaRepository.searchStudentClubDTO(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<StudentClubDTO> searchStudentV2(StudentSearchCondition condition, Pageable pageable) {
+        return studentRepository.searchAndPageWithCount(condition, pageable);
     }
 }
