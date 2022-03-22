517ed4630792f6c516bb6c86a4efff8ab54a004f
JinYeongKim <handayoung11@gmail.com>
2022-03-22 11:31:33 +0900

    StudentRepository interface가 QueryDslPredicateExecutor 상속

--- src/main/java/study/querydsl/repository/StudentRepository.java
+++ src/main/java/study/querydsl/repository/StudentRepository.java
@@ -1,10 +1,11 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Student;
 
 import java.util.List;
 
-public interface StudentRepository extends JpaRepository<Student, Long>, StudentDSLRepo {
+public interface StudentRepository extends JpaRepository<Student, Long>, StudentDSLRepo, QuerydslPredicateExecutor<Student> {
     List<Student> findByName(String name);
 }
